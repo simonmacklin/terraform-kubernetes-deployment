78e4bd1fbb33c6ebbd60abcef9923fd7bca30eba
Simon Macklin <simon@Simons-MacBook-Pro.local>
2023-02-18 20:01:14 +0000

    adding files

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,17 +1,13 @@
 
-data "aws_caller_identity" "current" {}
+data "aws_eks_cluster" "selected" {
+  name = "dev-k8s-fil"
+}
 
 data "aws_eks_cluster_auth" "selected" {
   name = data.aws_eks_cluster.selected.id
 }
 
-data "aws_eks_cluster" "selected" {
-  name = "dev-k8s-fil"
-}
-
-provider "aws" {
-
-}
+provider "aws" {}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.selected.endpoint
@@ -21,32 +17,12 @@ provider "kubernetes" {
 
 module "event_proxy" {
   source    = "../"
-  name      = "conductor-worker"
-  stack = "dev-k8s-fil"
+  name      = "simple-service"
+  stack     = "dev-k8s-fil"
   namespace = "default"
   image     = "nginx"
-  ports = [
-    {
-      name           = "app"
-      container_port = 80
-      service_port   = 5000
-    }
-  ]
   env = {
-    "hello"   = "a"
-    "ashello" = "v"
-  }
-  ingress_rules = [
-    {
-      name = "app"
-      path = "/"
-      host = "*"
-    }
-  ]
-  service_monitor = {
-    port     = 80
-    path     = "/metrics"
-    interval = "10s"
+    "KAFKA_HOST" = "kafka.data-platform"
   }
 }
 
